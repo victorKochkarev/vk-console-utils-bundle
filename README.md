471a1822c61708b0ae6456399cc7966e59bd365f
Victor Kochkarev <victor.kochkarev@gmail.com>
2017-11-18 21:28:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+CLI Utils Library
+=================
+
+Set of utilities designed to extend standard Symfony Command line functionality.
+
---
+++ src/Helper/ArrayObjectSelector.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace VK\CliUtils\Helper;
+
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+class ArrayObjectSelector
+{
+    public static function selectObjectFromArrayQuestion(InputInterface $input, OutputInterface $output, $itemList, string $displayPropertyName, string $questionText, $multipleSelect = false){
+        if(count($itemList) == 0){
+            throw new InvalidArgumentException('Item List is empty');
+        }
+
+        $questionHelper = new QuestionHelper();
+        $nameList = [];
+        $nameGetterName = 'get' . ucfirst($displayPropertyName);
+        foreach ($itemList as $item){
+            $nameList[] = $item->$nameGetterName();
+        }
+
+        if(count(array_unique($nameList)) != count($nameList)){
+            throw new \Exception('Input list items have duplicated names');
+        }
+
+        $question = new ChoiceQuestion(
+            $questionText,
+            $nameList
+        );
+
+        $question->setMultiselect($multipleSelect);
+
+        $selectedObjectName = $questionHelper->ask($input, $output, $question);
+        $selectedObjectNameList = [];
+        if(is_array($selectedObjectName)){
+            $selectedObjectNameList = $selectedObjectName;
+        }else{
+            $selectedObjectNameList = [$selectedObjectName];
+        }
+
+        if($multipleSelect){
+            $result = [];
+        }else{
+            $result = null;
+        }
+
+        foreach ($itemList as $item){
+            if(in_array($item->$nameGetterName(), $selectedObjectNameList)){
+                if($multipleSelect){
+                    $result[] = $item;
+                }else{
+                    $result = $item;
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
